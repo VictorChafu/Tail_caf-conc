d75d71a3c906aaeff259138ad5f8b669bc20cba2
VictorChafu <victorulloafreire@gmail.com>
2025-09-10 11:25:44 -0300

    letras

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+      play: ["Playfair Display", "serif"],
+      montserrat: ["Montserrat", "sans-serif"], // Fuente para títulos
+    },
+    },
   },
   plugins: [],
 }
